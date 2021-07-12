028e6672d2fc941b9e8b3cb971a9cffbdaec64eb
Richlau <617951086@qq.com>
2021-07-12 15:45:39 +0800

    lodash 0712 01

--- miao/lodash/richlau007-lodash.js
+++ miao/lodash/richlau007-lodash.js
@@ -14,16 +14,316 @@
     //   * 文件中的变量名全小写: var foobar = {chunk: function(){}}
     
 var richlau007 = function () {
-  function chunk() {
-    
+
+  //数组分组
+  function chunk(array, size = 1) {
+    let result = []
+    let arg = []
+    let s = size
+    for (let i = 0; i < array.length; i++){
+      arg.push(array[i])
+      if (--s == 0) {
+        result.push(arg)
+        arg = []
+        s = size
+      }
+    }
+    if (arg != []) result.push(arg)
+    return result
   }
+
+
+  // 压缩数组
+  function compact(array) {
+    let result = []
+    for (let i = 0; i < array.length; i++){
+      if (isNaN(array[i])) continue
+      else {
+        switch (array[i]) {
+          case false:
+          case null:
+          case 0:
+          case "":
+          case undefined:
+            continue;
+            break;
+          default:
+            result.push(array[i])
+            break;
+        }
+      }
+    }
+    return result
+  }
+
+  function uniq(array) {
+    let result = []
+    let obj = {}
+    for (var i = 0; i < array.length; i++){
+      if (array[i] in obj) continue
+      else {
+        obj[array[i]] = true
+        result.push(array[i])
+      }
+    }
+    return result
+  }
+
   function join() {
     
   }
 
 
   return {
-    chunk : chunk,
-    join : join
+    chunk,
+    compact,
+    concat,
+    difference,
+    differenceBy,
+    differenceWith,
+    join,
+    last,
+    lastIndexOf,
+    drop,
+    dropRight,
+    fill,
+    findIndex,
+    findLastIndex,
+    flatten,
+    flattenDeep,
+    flattenDepth,
+    fromPairs,
+    head,
+    indexOf,
+    initial,
+    reverse,
+    sortedIndex,
+    sortedIndexBy,
+    sortedIndexOf,
+    sortedLastIndex,
+    sortedLastIndexBy,
+    sortedLastIndexOf,
+    sortedUniq,
+    sortedUniqBy,
+    every,
+    filter,
+    find,
+    findLast,
+    max,
+    maxBy,
+    min,
+    minBy,
+    sum,
+    sumBy,
+    toArray,
+    lt,
+    lte,
+    eq,
+    gt,
+    gte,
+    castArray,
+    clone,
+    dropRightWhile,
+    dropWhile,
+    nth,
+    remove,
+    pull,
+    pullAll,
+    pullAllBy,
+    intersection,
+    intersectionBy,
+    intersectionWith,
+    pullAllWith,
+    pullAt,
+    tail,
+    take,
+    takeRight,
+    takeRightWhile,
+    takeWhile,
+    union,
+    unionBy,
+    unionWith,
+    uniq,
+    uniqBy,
+    uniqWith,
+    unzip,
+    unzipWith,
+    without,
+    xor,
+    xorBy,
+    xorWith,
+    zip,
+    zipObject,
+    zipWith,
+    countBy,
+    forEach,
+    forEachRight,
+    flatMap,
+    flatMapDeep,
+    flatMapDepth,
+    groupBy,
+    includes,
+    isArguments,
+    isArray,
+    isArrayBuffer,
+    isArrayLike,
+    isArrayLikeObject,
+    isBoolean,
+    isDate,
+    isElement,
+    isEmpty,
+    isEqual,
+    isEqualWith,
+    isError,
+    isFinite,
+    isFunction,
+    isInteger,
+    isLength,
+    isMap,
+    isMatch,
+    isMatchWith,
+    isNaN,
+    isNil,
+    isNull,
+    isNumber,
+    isObject,
+    isObjectLike,
+    isPlainObject,
+    isRegExp,
+    isSafeInteger,
+    isSet,
+    isString,
+    isSymbol,
+    isTypedArray,
+    isUndefined,
+    isWeakMap,
+    isWeakSet,
+    toFinite,
+    toInteger,
+    toLength,
+    toNumber,
+    invokeMap,
+    keyBy,
+    map,
+    partition,
+    reduce,
+    reduceRight,
+    reject,
+    sample,
+    sampleSize,
+    shuffle,
+    size,
+    some,
+    defer,
+    delay,
+    conformsTo,
+    toSafeInteger,
+    add,
+    divide,
+    mean,
+    meanBy,
+    multiply,
+    subtract,
+    clamp,
+    inRange,
+    random,
+    assign,
+    assignIn,
+    defaults,
+    defaultsDeep,
+    findKey,
+    findLastKey,
+    forIn,
+    forInRight,
+    forOwn,
+    forOwnRight,
+    functions,
+    functionsIn,
+    invert,
+    invertBy,
+    keys,
+    keysIn,
+    mapKeys,
+    mapValues,
+    merge,
+    mergeWith,
+    omit,
+    omitBy,
+    pick,
+    pickBy,
+    toPairs,
+    toPairsIn,
+    ceil,
+    floor,
+    round,
+    at,
+    get,
+    has,
+    hasIn,
+    invoke,
+    result,
+    set,
+    transform,
+    unset,
+    update,
+    setWith,
+    updateWith,
+    values,
+    valuesIn,
+    camelCase,
+    capitalize,
+    endsWith,
+    escape,
+    escapeRegExp,
+    kebabCase,
+    lowerCase,
+    lowerFirst,
+    pad,
+    padEnd,
+    padStart,
+    repeat,
+    replace,
+    snakeCase,
+    split,
+    startCase,
+    startsWith,
+    toLower,
+    toUpper,
+    trim,
+    trimEnd,
+    trimStart,
+    orderBy,
+    sortBy,
+    parseInt,
+    truncate,
+    unescape,
+    upperCase,
+    upperFirst,
+    words,
+    defaultTo,
+    range,
+    rangeRight,
+    times,
+    toPath,
+    uniqueId,
+    cloneDeep,
+    identity,
+    ary,
+    unary,
+    curry,
+    once,
+    negate,
+    spread,
+    matches,
+    property,
+    propertyOf,
+    flip,
+    conforms,
+    constant,
+    nthArg,
+    flow,
+    method,
+    methodOf,
+    parseJson,
   }
 }()
